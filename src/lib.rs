e8838860ac61a99dbd577e2fa04d0e4b87dbc302
Maxim Kosterov <maxim.kosterov@gmail.com>
2021-02-18 21:45:22 +0300

    Added logging macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 mod bench;
 mod global;
 
-pub use global::deinstantiate;
-pub use global::instantiate;
-
-pub use global::begin_time;
-pub use global::file_mutex;
+pub(crate) use global::begin_time;
+pub(crate) use global::deinstantiate;
+pub(crate) use global::file_mutex;
+pub(crate) use global::instantiate;
 
 pub use bench::bench;
 pub use bench::log;
@@ -15,6 +14,7 @@ pub use bench::TimeScope;
 #[macro_export]
 macro_rules! scope {
     ($name: ident) => {
+        use gbench::TimeScope;
         let $name = TimeScope::new(stringify!($name));
     };
 }
@@ -22,6 +22,16 @@ macro_rules! scope {
 #[macro_export]
 macro_rules! instantiate {
     ($folder:expr) => {
+        use gbench::Instantiator;
         let __instantiator__ = Instantiator::new($folder);
+    };
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        use gbench::log;
+        let log_string = format!($($arg)*);
+        log(&log_string);
     };
 }
